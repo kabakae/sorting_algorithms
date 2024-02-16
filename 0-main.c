746f181e73e693676ebe5251a8fa2570f72e3291
kabakae <kabakaevans@gmail.com>
2024-02-16 05:48:40 -0800

    H

---
+++ 0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {706, 540, 230, 910, 100, 67,950, 120, 777,560};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
