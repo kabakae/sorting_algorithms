b6cf7b229aeadbaf0a56a5bcb5b1815fd7164188
kabakae <kabakaevans@gmail.com>
2024-02-16 05:43:14 -0800

    H

--- sort.h
+++ sort.h
@@ -43,5 +43,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 #endif /* SORT_H */
